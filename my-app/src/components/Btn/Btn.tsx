a5fccddd1f8d81e27224e60ecec279d8b5a7f2ab
Bernard Krehula <krehula.bernard456@gmail.com>
2025-09-21 13:16:20 +0200

    Define btn component

---
+++ my-app/src/components/Btn/Btn.tsx
@@ -0,0 +1,16 @@
+import type { MouseEventHandler } from "react"
+
+type BtnTypes = {
+    variation: string;
+    children: string;
+    onClick: MouseEventHandler<HTMLButtonElement>
+}
+
+const Btn = ({variation, onClick, children}: BtnTypes) => {
+
+    return(
+        <button onClick={onClick} className={`btn ${variation}`}>{children}</button>
+    )
+}
+
+export default Btn;
