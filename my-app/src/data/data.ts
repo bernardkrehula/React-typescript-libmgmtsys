06d2a37a97187a34ae0241f60b2fc60b6a98ac8d
Bernard Krehula <krehula.bernard456@gmail.com>
2025-09-21 16:52:37 +0200

    create data

---
+++ my-app/src/data/data.ts
@@ -0,0 +1,101 @@
+
+const data = {
+    defaultLoginInfo: {
+        email: 'testadmin@library.com',
+        password: 'testpassword'
+    },
+    books: [
+        {
+            id: '1001',
+            title: 'In Search of Lost Time',
+            author: 'Marcel Proust	',
+            status: 'Available'
+        },
+        {
+            id: '1002',
+            title: 'Ulysses',
+            author: 'James Joyce',
+            status: 'Issued'
+        },
+        {
+            id: '1003',
+            title: 'Don Quixote',
+            author: 'Miguel de Cervantes',
+            status: 'Available'
+        },
+        {
+            id: '1004',
+            title: 'The Great Gatsby',
+            author: 'F. Scott Fitzgerald',
+            status: 'Issued'
+        },
+        {
+            id: '1005',
+            title: 'War and Peace',
+            author: 'Leo Tolstoy',
+            status: 'Available'
+        },
+        {
+            id: '1006',
+            title: 'Hamlet',
+            author: 'William Shakespeare',
+            status: 'Available'
+        },
+        {
+            id: '1007',
+            title: 'Crime and Punishment',
+            author: 'Fyodor Dostoyevsky',
+            status: 'Issued'
+        },
+        {
+            id: '1008',
+            title: 'Pride and Prejudice',
+            author: 'Jane Austen',
+            status: 'Available'
+        },
+        {
+            id: '1009',
+            title: 'The Iliad',
+            author: 'Homer',
+            status: 'Available'
+        },
+        {
+            id: '1010',
+            title: 'The Alchemist',
+            author: 'Paulo Coelho',
+            status: 'Available'
+        }
+    ],
+    members: [
+        {
+            id: '101',
+            name: 'John Doe',
+            phone: '132-465-8791',
+            email: 'john@doe.com',
+            fine: '0',
+        },
+        {
+            id: '102',
+            name: 'Jane Doe',
+            phone: '789-456-1123',
+            email: 'janedoe@mail.com',
+            fine: '100',
+        },
+        {
+            id: '103',
+            name: 'Anamika',
+            phone: '456-132-4892',
+            email: 'anamika@gmail.com',
+            fine: '40',
+        },
+        {
+            id: '104',
+            name: 'Joe Shmoe',
+            phone: '123-465-7890',
+            email: 'joe@sc.co',
+            fine: '85',
+        }
+    ]
+}
+
+export default data;
