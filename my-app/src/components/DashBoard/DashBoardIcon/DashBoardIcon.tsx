cd3fe89a3d0d231276e770d0ac74ca7c52d7932b
Bernard Krehula <krehula.bernard456@gmail.com>
2025-09-21 18:58:31 +0200

    Menu styled

--- my-app/src/components/DashBoard/DashBoardIcon/DashBoardIcon.tsx
+++
@@ -1,7 +0,0 @@
-
-const DashboardIcon = () => {
-
-    
-}
-
-export default DashboardIcon;
--- my-app/src/components/Menu/Menu.tsx
+++ my-app/src/components/Menu/Menu.tsx
@@ -1,8 +1,8 @@
-import DashboardIcon from '../DashBoard/DashBoardIcon/DashBoardIcon';
 import './Menu.css'
 import { Link } from 'react-router-dom';
 import data from '../../data/data';
 import parse from "html-react-parser";
+import Btn from '../Btn/Btn';
 
 const Menu = () => {
     const defaultIconsData = data.defaultIconElements;
@@ -10,7 +10,7 @@ const Menu = () => {
     const displayLinks = () => {
         return Object.values(defaultIconsData).map(icon => {
             const { name, svg, link } = icon;
-            console.log(svg)
+            
             return(
                 <div className='icon'>
                     {parse(svg)}
@@ -24,7 +24,7 @@ const Menu = () => {
         <div className="menu">
             <div className='logo'>
                 <div className='logo-content'>
-                    <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeLarge css-6flbmm" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="LocalLibraryIcon"><path d="M12 11.55C9.64 9.35 6.48 8 3 8v11c3.48 0 6.64 1.35 9 3.55 2.36-2.19 5.52-3.55 9-3.55V8c-3.48 0-6.64 1.35-9 3.55zM12 8c1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3 1.34 3 3 3z"></path></svg>
+                    <svg className="logo-svg" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="LocalLibraryIcon"><path d="M12 11.55C9.64 9.35 6.48 8 3 8v11c3.48 0 6.64 1.35 9 3.55 2.36-2.19 5.52-3.55 9-3.55V8c-3.48 0-6.64 1.35-9 3.55zM12 8c1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3 1.34 3 3 3z"></path></svg>
                     <h1>Lms</h1>
                 </div>
                 <div className='horizontal-line'></div>
@@ -33,6 +33,10 @@ const Menu = () => {
             <div className='icons'>
                 {displayLinks()}
             </div>
+            <Btn variation='logout'>
+                Logout
+                <svg className="logout-svg" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="LogoutIcon"><path d="m17 7-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"></path></svg>
+            </Btn>
         </div>
     )
 }
