99f09b39c399f5dccd3f4e8fab33c20ec930057e
Bernard Krehula <krehula.bernard456@gmail.com>
2025-09-21 13:00:18 +0200

    Login window created

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,25 +5,37 @@ import Members from './components/Members/Members'
 import Menu from './components/Menu/Menu'
 import DefaultPage from './components/DefaultPage/DefaultPage'
 import Book from './components/Books/Book/Book'
+import { useState } from 'react'
+import LoginWindow from './components/LoginWindow/LoginWindow'
 
 function App() {
+  const [ userLogged, isUserLogged ] = useState(false);
 
-  return (
-    <>
-    <BrowserRouter>
-      <div className='main'>
-        <Menu /> 
-        <div className='content'>
-          <Routes>
-            <Route path='/' element={<DefaultPage />}></Route>
-            <Route path='/Books' element={<Books />}/>
-            <Route path='/Members' element={<Members />}/>
-          </Routes>
-        </div>
-      </div>
-      </BrowserRouter>
-    </>
-  )
+  if(!userLogged){
+    return(
+      <>
+        <LoginWindow />
+      </>
+    )
+  } 
+  else{
+    return (
+      <>
+        <BrowserRouter>
+          <div className='main'>
+            <Menu /> 
+            <div className='content'>
+              <Routes>
+                <Route path='/' element={<DefaultPage />}></Route>
+                <Route path='/Books' element={<Books />}/>
+                <Route path='/Members' element={<Members />}/>
+              </Routes>
+            </div>
+          </div>
+          </BrowserRouter>
+      </>
+    )
+  }
 }
 
 export default App
