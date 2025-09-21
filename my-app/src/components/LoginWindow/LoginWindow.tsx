6e89be99b3c15c94286030f6e046e662ad184a55
Bernard Krehula <krehula.bernard456@gmail.com>
2025-09-21 16:40:48 +0200

    Login page styled

--- my-app/src/components/LoginWindow/LoginWindow.tsx
+++ my-app/src/components/LoginWindow/LoginWindow.tsx
@@ -14,8 +14,14 @@ const LoginWindow = () => {
                 <SingleInput /* onClick={} */ placeholder='' variation='login-input' ></SingleInput>
                 <Btn onClick={() => console.log('radi')} variation='login-btn'>Login</Btn>
             </div>
-            
-            
+            <div className='login-info'>
+                <svg className="info-svg" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="InfoOutlinedIcon"><path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20, 12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10, 10 0 0,0 12,2M11,17H13V11H11V17Z"></path></svg>
+                <div>
+                    <h3>Use login info</h3>
+                    <h4>Email Address: <span>testadmin@library.com</span></h4>
+                    <h4>Password: <span>testpassword</span></h4>
+                </div>
+            </div>
         </div>
     )
 }
