f6a34b27f6d033eda657c18ea11d32575d5a1c5d
Bernard Krehula <krehula.bernard456@gmail.com>
2025-09-22 18:53:57 +0200

    display modal

--- my-app/src/components/DashBoard/DashBoard.tsx
+++ my-app/src/components/DashBoard/DashBoard.tsx
@@ -1,17 +1,29 @@
 import './Dashboard.css'
+import Modal from './Modal';
 
 type DashBoardTypes = {
-    data: string[];
+    data: Object;
 }
 
 const Dashboard = ({data}: DashBoardTypes) => {
-    console.log(data)
+    
+    const displayModals = () => {
+        return Object.values(data.defaultIconElements).reverse().map((icon, index) => {
+            const { name, svg, link } = icon;
+
+            const listsLength = data[name.toLowerCase()];
+
+            if(listsLength) return(
+                <Modal key={index} name={name} svg={svg} link={link} length={listsLength.length}/>
+            )
+        })
+    }
 
     return(
         <div className="dashboard">
            <h1>Dashboard</h1> 
            <div className='options'>
-            
+                {displayModals()}
            </div>
         </div>
     )
--- my-app/src/components/Dashboard/Modal.tsx
+++ my-app/src/components/Dashboard/Modal.tsx
@@ -1,4 +1,23 @@
+import parse from "html-react-parser";
 
-const Modal = () => {
-    
+type ModalTypes = {
+    name: string;
+    svg: string;
+    link: string;
+    length: number;
 }
+
+const Modal = ({name, svg, link, length}: ModalTypes) => {
+    
+    if(name === 'Dashboard') return null;
+
+    return(
+        <div className="modal">
+            {parse(svg)}
+            {name}
+            {length}
+        </div>
+    )
+}
+
+export default Modal;
